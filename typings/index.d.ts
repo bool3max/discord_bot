3922ead9439e11dc7e77d4be1c3241bbd943cba1
Bogdan Mitrovic <bokisa.mitrovic2@gmail.com>
2016-10-31 19:33:02 +0100

    deleted typings ase they are no longer needed

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/koa/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/redis/index.d.ts" />
-/// <reference path="globals/ws/index.d.ts" />
