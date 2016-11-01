e95fec3a0981ab97c483cae5e1830815dfb45e2b
Bogdan Mitrovic <bokisa.mitrovic2@gmail.com>
2016-11-01 19:59:42 +0100

    moved the redis server password into bot_config.json as redis_pass

--- src/db/database.js
+++ src/db/database.js
@@ -2,11 +2,13 @@ import redis from 'redis';
 import {promisifyAll} from 'bluebird';
 import r_handler from '../utils/reject_handler';
 
+const {redis_pass} = require('../bot_config.json');
+
 promisifyAll(redis.RedisClient.prototype);
 promisifyAll(redis.Multi.prototype);
 
 const db = redis.createClient({
-	password: 'a45hgf678nopzx48712yy3cc0',
+	password: redis_pass,
 	string_numbers: false
 });
 
@@ -15,7 +17,7 @@ db.on('error', r_handler);
 export default db;
 export function createSubscriber(channel, onMessage) {
 	const sub = redis.createClient({
-		password: 'a45hgf678nopzx48712yy3cc0'
+		password: redis_pass
 	});
 	sub.on('message', onMessage);
 	sub.subscribe(channel);
